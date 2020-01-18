3f6acdaf64f3a48a6d47526e7d67f6244c63bf38
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-18 18:44:02 -0400

    Loading e disabled

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
@@ -22,10 +22,12 @@ export default class Main extends Component {
     state = {
         newUser: '',
         users: [],
+        loading: false,
     };
 
     handleAddUser = async () => {
         const {users, newUser} = this.state;
+        this.setState({loading: true});
         const response = await api.get(`/users/${newUser}`);
         const data = {
             name: response.data.name,
@@ -33,12 +35,12 @@ export default class Main extends Component {
             bio: response.data.bio,
             avatar: response.data.avatar_url,
         };
-        this.setState({users: [...users, data], newUser: ''});
+        this.setState({users: [...users, data], newUser: '', loading: false});
         Keyboard.dismiss();
     };
 
     render() {
-        const {newUser, users} = this.state;
+        const {newUser, users, loading} = this.state;
         return (
             <Container>
                 <Form>
@@ -51,8 +53,14 @@ export default class Main extends Component {
                         returnKeyType="send"
                         onSubmitEditing={this.handleAddUser}
                     />
-                    <SubmitButton onPress={this.handleAddUser}>
-                        <Icon name="add" size={20} color="#fff" />
+                    <SubmitButton
+                        loading={loading}
+                        onPress={this.handleAddUser}>
+                        {loading ? (
+                            <ActivityIndicator color="#fff" />
+                        ) : (
+                            <Icon name="add" size={20} color="#fff" />
+                        )}
                     </SubmitButton>
                 </Form>
                 <List
