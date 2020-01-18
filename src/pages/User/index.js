86424a43a890d7cf3de0cd49e0c57f667c062805
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-18 19:47:31 -0400

    Buscando dados da API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
+import PropTypes from 'prop-types';
 
-export default function User() {
-    return <View />;
+import api from '../../services/api';
+
+export default class User extends Component {
+    static navigationOptions = ({navigation}) => ({
+        title: navigation.getParam('user').name,
+    });
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            starts: [],
+        };
+    }
+
+    async componentDidMount() {
+        const {navigation} = this.props;
+        const user = navigation.getParam('user');
+        const response = await api.get(`/users/${user.login}/starred`);
+        this.setState({starts: response.data});
+    }
+
+    render() {
+        const {starts} = this.state;
+        return <View />;
+    }
 }
+
+User.propTypes = {
+    navigation: PropTypes.shape({
+        getParam: PropTypes.func,
+    }).isRequired,
+};
